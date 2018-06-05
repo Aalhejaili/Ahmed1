ac51227d7024e25113a81a66f6983b9e8accc55c
aalhejaili <aalhejaili@ucdavis.edu>
2018-06-04 18:15:35 -0700

    initial commit

--- cllient/src/components/common/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-    return(
-        <div className="py-3 text-center">
-            <i className="fas fa-cog fa-spin fa-2x" />
-        </div>
-    );
-};
--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:'mongodb://admin:admin@ds157089.mlab.com:57089/sabic18'
+    mongoURI:'mongodb://admin:admin123@ds247690.mlab.com:47690/sabic18',
     secretOrKey: 'secret'
 }
